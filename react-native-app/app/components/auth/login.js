68471f7fa6cd19baa6e597a25d610ddf94db3fb7
nickboone <nickboon@student.arteveldehs.be>
2019-05-06 23:14:23 +0200

    change

--- react-native-app/app/components/auth/login.js
+++ react-native-app/app/components/auth/login.js
@@ -1,8 +1,3 @@
-/**
- * Author: Moses Adekunle Esan for E&M Digital
- * Date: 7/27/2017
- * Project: How to Build a React Native/Redux app using a JWT-Powered API.
- */
 
 'use strict';
 
--- react-native-app/app/store.js
+++ react-native-app/app/store.js
@@ -1,8 +1,3 @@
-/**
- * Author: Moses Adekunle Esan for E&M Digital
- * Date: 2/3/2017
- * Project: React Native Redux Boilerplate
- */
 
 'use strict';
 
