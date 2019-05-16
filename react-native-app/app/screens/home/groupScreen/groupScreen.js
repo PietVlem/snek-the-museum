ccc6cc16181eb3e959789116248da47dd4dd8c33
nickboone <nickboon@student.arteveldehs.be>
2019-05-16 15:42:38 +0200

    Group create page DONE

--- react-native-app/app/screens/home/groupScreen/groupScreen.js
+++ react-native-app/app/screens/home/groupScreen/groupScreen.js
@@ -2,10 +2,11 @@
  
 
 import React, {Component} from 'react';
-var { View, Text, AsyncStorage,Image,TouchableOpacity,FlatList,ScrollView } = require('react-native');
+var { View, Text, AsyncStorage,Image,TouchableOpacity,FlatList,ScrollView,StyleSheet } = require('react-native');
 import * as Animatable from 'react-native-animatable';
 import {connect} from 'react-redux';
-
+import { NavBar  } from '../../index';
+import RNPickerSelect from 'react-native-picker-select';
 import {Actions} from 'react-native-router-flux';
 import { Icon } from 'react-native-elements'
 import {setStatus, logout} from '../../../actions/auth'; //Import your actions
@@ -14,6 +15,18 @@ import styles from './style' //Import your styles
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { ListItem } from 'react-native-elements'
 class groupScreen extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.inputRefs = {};
+        this.state = {
+            type: "",
+            price: "",
+            visited: ""
+        };
+        this.onChange = this.onChange.bind(this);
+    }
 
     componentDidMount() {
         var _this = this;
@@ -24,29 +37,31 @@ class groupScreen extends Component {
         //     else _this.props.setStatus(true)
         // });
     }
+
+    onChange(e, fieldName) {
+        const {text} = e.nativeEvent
+        this.setState({ [fieldName] : text });
+    }
+
+
+
     renderRow ({ item }) {
         return (
-        <TouchableOpacity>
           <ListItem
             roundAvatar
             title={item.name}
-            subtitle={item.subtitle}
-            avatar={{uri:item.avatar_url}}
             containerStyle={styles.Liststyle}
-            subtitleStyle={styles.subtitle}
             titleStyle={styles.ListItemTitle}
             rightIcon={
                 <Icon
-                name='ios-arrow-forward'
+                name='ios-close'
                 type='ionicon'
                 color='#6FA29B'
-                size={15}
+                size={25}
                 iconStyle={{paddingRight: 15,}}
                 onPress={() => console.log('hello')} />
             }
-            chevronColor="#6FA29B"
-          />
-        </TouchableOpacity>   
+          />  
         )
       }
 
@@ -54,17 +69,21 @@ class groupScreen extends Component {
         const list = [
             {
               name: 'Amy Farha',
-              avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-              subtitle: 'Vice President'
             },
             {
               name: 'Chris Jackson',
-              avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-              subtitle: 'Vice Chairman'
+            },
+            {
+                name: 'Chris Jackson',
+            },
+            {
+                name: 'Chris Jackson',
             },
           ]
         return (
-            <ScrollView style={{flex: 1,backgroundColor: "#FFF",marginHorizontal: 20,}}>
+            <ScrollView style={{flex: 1,backgroundColor: "#FFF",marginTop: -30,}}>
+                <View>
+                    <NavBar/>
                     <Text style={styles.GroupCreateTitle}>Snake groep maken</Text> 
                         <FlatList
                         ref='listRef'
@@ -73,7 +92,112 @@ class groupScreen extends Component {
                         renderItem={this.renderRow}
                         initialNumToRender={5}
                         keyExtractor={(item, index) => index.toString()}/>            
-                    {
+                </View>
+                <Image
+                            style={styles.SnakeLayout}
+                            source={require('../../../../assets/logo.png')}
+                        />
+                <View style={{marginBottom: 40,}}>
+                    <Text style={styles.FilterTitle}>Voorkeuren</Text> 
+                    <Text style={styles.labelSelect}>Type</Text>
+                    <RNPickerSelect
+                        placeholder={{
+                            label: 'Type',
+                            value: null,
+                        }}
+                        items={[
+                            {
+                                label: 'Design gericht',
+                                value: 'design',
+                            },
+                            {
+                                label: 'Kindvriendelijk',
+                                value: 'child',
+                            },
+                            {
+                                label: 'Nieuw',
+                                value: 'new',
+                            },
+                            {
+                                label: 'Historiek',
+                                value: 'history',
+                            },
+                        ]}
+                        onValueChange={(value) => {
+                            this.setState({
+                                type: value,
+                            });
+                        }}
+                        style={{ ...pickerSelectStyles}}
+                        value={this.state.type}
+                        ref={(el) => {
+                            this.inputRefs.picker = el;
+                        }}
+                    />
+                    <Text style={styles.labelSelect}>Prijs</Text>
+                    <RNPickerSelect
+                        placeholder={{
+                            label: 'Prijs',
+                            value: null,
+                        }}
+                        items={[
+                            {
+                                label: 'minder dan 10 €',
+                                value: 'min',
+                            },
+                            {
+                                label: 'meer dan 10 €',
+                                value: 'more',
+                            },
+                        ]}
+                        onValueChange={(value) => {
+                            this.setState({
+                                price: value,
+                            });
+                        }}
+                        style={{ ...pickerSelectStyles}}
+                        value={this.state.price}
+                        ref={(el) => {
+                            this.inputRefs.picker = el;
+                        }}
+                    />
+                    <Text style={styles.labelSelect}>Museum</Text>
+                    <RNPickerSelect
+                        placeholder={{
+                            label: 'Bezocht',
+                            value: null,
+                        }}
+                        items={[
+                            {
+                                label: 'Eerder bezocht',
+                                value: 'design',
+                            },
+                            {
+                                label: 'Niet eerder bezocht',
+                                value: 'child',
+                            },
+                        ]}
+                        onValueChange={(value) => {
+                            this.setState({
+                                visited: value,
+                            });
+                        }}
+                        style={{ ...pickerSelectStyles}}
+                        value={this.state.visited}
+                        ref={(el) => {
+                            this.inputRefs.picker = el;
+                        }}
+                    />
+                </View>
+                <TouchableOpacity onPress={this.props.onPress} style={styles.btnContainer}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            GROEP BEVESTIGEN
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                    
+                {
                     // (this.props.loggedIn) &&
                     // <View>
                     //     <Text style={[styles.welcomeText]}>Welcome</Text>
@@ -85,6 +209,27 @@ class groupScreen extends Component {
         );
     }
 };
+
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        width: "90%",
+        marginVertical: 10,
+        borderBottomColor: 'white',
+        backgroundColor: "white", 
+        shadowOffset:{  width: 4,  height: 6,  },
+        shadowColor: '#8386A3',
+        shadowOpacity: 0.12,
+        borderRadius: 10,
+        paddingTop: 20,
+        paddingBottom: 20,
+        marginHorizontal: 20,
+        paddingLeft: 30,
+        fontSize: 14,
+        fontFamily:"MontserratMedium",
+        color: "#303E48",
+    },
+});
 
 
 function mapStateToProps(state, props) {
--- react-native-app/app/screens/home/groupScreen/style.js
+++ react-native-app/app/screens/home/groupScreen/style.js
@@ -9,6 +9,89 @@ var styles = StyleSheet.create({
         fontFamily: 'PTSansBold',
         fontSize: 17,
         marginBottom: 20,
+        marginLeft: 20,
+        marginTop: 20
+    },
+    SnakeLayout:{
+        width: 100,
+        resizeMode: 'contain',
+        height: 100,
+        position: "absolute",
+        left: "-17%",
+        top: "40%",
+        transform: [{ rotate: '-310deg'}]
+    },
+    Liststyle:{
+        marginTop: 10,
+        borderBottomWidth: 0,
+        backgroundColor: "white",
+        borderRadius: 10,
+        shadowColor: "#8386A3",
+        shadowOffset: {
+            width: 2,
+            height: 4,
+        },
+        shadowOpacity: 0.15,
+        shadowRadius: 3.84,
+        elevation: 1,
+        paddingTop: 20,
+        paddingBottom: 20,
+    },
+    Listbox:{
+        paddingHorizontal: 20,
+        paddingBottom: 20,
+    },
+    subtitle:{
+        color: "#A9A9B0",
+        fontFamily: 'MontserratLight',
+        fontSize: 12,
+        fontWeight: "300",
+        paddingLeft: 10
+    },
+    ListItemTitle:{
+        color: "#303E48",
+        fontFamily: 'MontserratMedium',
+        fontSize: 15,
+        paddingLeft: 10
+    },
+    FilterTitle:{
+        color: "#3E4A59",
+        fontFamily: 'PTSansBold',
+        fontSize: 17,
+        marginBottom: 20,
+        marginLeft: 20,
+        marginTop: 100,
+    },
+    labelSelect:{
+        marginLeft: 30,
+        color: "#303E48",
+        fontSize: 12,
+        fontFamily: "MontserratMedium",
+        marginTop: 30,
+    },
+    button:{
+        height: 52,
+        marginTop: 10,
+        backgroundColor: '#E7B164',
+        justifyContent: "center",
+        alignSelf: 'center',
+        borderRadius:50,
+        width: '70%',
+        shadowColor: "#C7D0F8",
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        shadowOffset: {
+          height: 3,
+          width: 0
+        },
+        marginBottom: 80,
+    },
+
+    buttonText:{
+        fontFamily: 'PTSansBold',
+        fontSize: 16,
+        color: "#fff",
+        textAlign: "center"
     },
 
 });
