1065358977f62e59ecf8bce718ac09b613159a32
nickboone <nickboon@student.arteveldehs.be>
2019-06-13 16:36:06 +0200

    Login, Register, Password recover

--- react-native-app/app/api/auth.js
+++ react-native-app/app/api/auth.js
@@ -1,5 +1,5 @@
 
-var { AsyncStorage } = require('react-native');
+var { AsyncStorage,Alert } = require('react-native');
 import { Actions } from 'react-native-router-flux';
 import { SERVER_ERROR, ERROR } from "../config";
 
@@ -9,8 +9,9 @@ var REQUEST_URL = "http://127.0.0.1:8080/api/v1";
 // Callback : success, data, error
 var AuthAPI = {
     register: function (data, callback) {
-        var url = REQUEST_URL + "/singUp";
-        this.requestWithoutToken(url, data, callback);
+        var url = REQUEST_URL + "/signUp";
+        console.log(data)
+        this.requestRegister(url, data, callback);
     },
 
     login: async function (data, callback) {
@@ -18,18 +19,19 @@ var AuthAPI = {
         await this.requestWithoutToken(url, data, callback);
     },
 
-    async logout(callback) {
+    logout: async function (callback) {
         await this.deleteDataAsyncStorage();
     },
 
     deleteDataAsyncStorage: async function(){
         await AsyncStorage.removeItem('token');
-        Actions.login();
+        const token = await AsyncStorage.getItem('token');
+        if (!token){Actions.login();}
     },
 
     recover: function (data, callback) {
-        var url = REQUEST_URL + "/recover";
-        this.requestWithoutToken(url, data, callback);
+        var url = REQUEST_URL + "/forgot";
+        this.requestRecover(url, data, callback);
     },
 
     requestWithoutToken(url, data, callback) {
@@ -61,13 +63,65 @@ var AuthAPI = {
             .done();
     },
 
+    requestRecover(url, data, callback) {
+        let requestConfig = {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: data.email,
+            })
+        }
+
+        fetch(url, requestConfig)
+            .then((response) => response.json())
+            .then( async (responseData) => {
+                Alert.alert('Er is een reset e-mail verzonden naar dit emailadres.');
+            })
+            .catch(error => {
+                callback(false, null, { type: SERVER_ERROR })
+            })
+            .done();
+    },
+
+    requestRegister(url, data, callback) {
+        let requestConfig = {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: data.email,
+                password: data.password,
+                dayOfBirth: data.age,
+                name: data.username,
+            })
+        }
+
+        fetch(url, requestConfig)
+            .then((response) => response.json())
+            .then( async (responseData) => {
+                const token = await AsyncStorage.getItem('token');
+                if (!token){Actions.login();}
+            })
+            .catch(error => {
+                callback(false, null, { type: SERVER_ERROR })
+            })
+            .done();
+    },
+
     saveDataAsyncStorage: async function(responseData){
-        await AsyncStorage.multiSet([
-            ["token", responseData.token],
-            ["userId", responseData.userId]
-        ])
-        const token = await AsyncStorage.getItem('token');
-        if (token){Actions.home();}
+        if (responseData.token && responseData.userId){
+            await AsyncStorage.multiSet([
+                ["token", responseData.token],
+                ["userId", responseData.userId]
+            ])
+            const token = await AsyncStorage.getItem('token');
+            if (token){Actions.home();}
+        }
     },
 }
 
